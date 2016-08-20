6e68263eda4729a6aaab63b7db46332486b58b64
“kasper341” <kasper_341@hotmail.com>
2016-08-19 20:42:27 -0700

    Add TodoList schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820030329) do
+ActiveRecord::Schema.define(version: 20160820034132) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20160820030329) do
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string   "list_name"
+    t.date     "list_due_date"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
