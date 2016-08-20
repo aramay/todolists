c3ad8070216f31ed3be7710a66ca4637d138c95a
“kasper341” <kasper_341@hotmail.com>
2016-08-19 20:24:31 -0700

    Add schema for profile Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820024324) do
+ActiveRecord::Schema.define(version: 20160820030329) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "gender"
+    t.integer  "birth_year"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
