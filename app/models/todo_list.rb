7edc3e7faabfa290b72467f7caf1c0615364f541
“kasper341” <kasper_341@hotmail.com>
2016-08-20 13:42:35 -0700

    Fix many-to-many associations for User and TodoItems

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,5 @@
 class TodoList < ActiveRecord::Base
-   belongs_to :user
+   # belongs_to :user
+   has_and_belongs_to_many :users
    has_many :todo_items, dependent: :destroy
-   # has_and_belongs_to_many :users
 end
