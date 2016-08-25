cfb562b05f948201e549ad0072ee846198237fa1
“kasper341” <kasper_341@hotmail.com>
2016-08-25 12:00:11 -0700

    Update application model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
    has_one :profile
    # has_many :todo_lists
-   has_and_belongs_to_many :todo_lists
+   # has_and_belongs_to_many :todo_lists
+   has_many :todo_lists
 
    has_many :todo_items, through: :todo_lists, source: :todo_items
    # has_many :todo_lists, through: :todo_lists_users
