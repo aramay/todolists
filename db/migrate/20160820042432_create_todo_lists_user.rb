477ce467b4826574ac01e7c376dd537e4d84bec8
“kasper341” <kasper_341@hotmail.com>
2016-08-25 12:00:32 -0700

    Update application schema and migration

--- db/migrate/20160820042432_create_todo_lists_user.rb
+++
@@ -1,8 +0,0 @@
-class CreateTodoListsUser < ActiveRecord::Migration
-  def change
-    create_table :todo_lists_users, id: false do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :todo_list, index: true, foreign_key: true
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -40,10 +40,12 @@ ActiveRecord::Schema.define(version: 20160820050413) do
   create_table "todo_lists", force: :cascade do |t|
     t.string   "list_name"
     t.date     "list_due_date"
-    t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
+
+  add_index "todo_lists", ["user_id"], name: "index_todo_lists_on_user_id"
 
   create_table "todo_lists_users", id: false, force: :cascade do |t|
     t.integer "user_id"
