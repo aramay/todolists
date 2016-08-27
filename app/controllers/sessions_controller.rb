b094e74c61686c139ea51426f2ea5a176ac92a48
“kasper341” <kasper_341@hotmail.com>
2016-08-27 08:47:54 -0700

    Add login workflow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+   #   binding.pry
+     user = User.find_by(username: params[:user][:username])
+
+     password = params[:user][:password]
+
+     if user && user.authenticate(password)
+        session[:user_id] = user.id
+        redirect_to root_path, notice: "Logged in successfully"
+     else
+        redirect_to login_path, alert: "Invalid username/password"
+     end
+
+
   end
 
   def destroy
+     reset_session #clear session cookies
+     redirect_to login_path, notice: "You have logged out"
   end
 end
